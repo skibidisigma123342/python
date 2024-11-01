3824353d55e1baa0fcbe0340e272c0bef25ccbf6
nikit <nikita.kamianoi@gmail.com>
2024-11-01 19:58:10 +0100

    kwadratik....kakwakadrik

---
+++ pythonik.py
@@ -0,0 +1,8 @@
+class Rectangle:
+    def __init__(self, height=160, width=140):
+        self.height = height
+        self.width = width
+
+
+calculate_area = Rectangle(width=140), Rectangle(height=170)
+calculate_perimeter = Rectangle
