253e17841bbd345b8c338f80a470e9a9c34b6375
nikit <nikita.kamianoi@gmail.com>
2024-11-01 19:47:28 +0100

    cars....skibidi...

--- yesSir.py
+++ yesSir.py
@@ -1,1 +1,13 @@
-print("Hello")
+class Product:
+
+    def __init__(self):
+        self.Calculate_total_price = 20
+
+
+    def quantily(self):
+        print(self.quantily)
+
+
+
+display_info = Product()
+display_info.quantily()
